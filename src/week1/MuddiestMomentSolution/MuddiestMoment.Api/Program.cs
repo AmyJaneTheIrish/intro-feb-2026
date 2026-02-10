030960a4fbe84af4c72027a3a8dd6a4342117c30
AmyJane Reilly <amyjane.reilly@gmail.com>
2026-02-10 16:40:32 -0500

    More API

--- src/week1/MuddiestMomentSolution/MuddiestMoment.Api/Program.cs
+++ src/week1/MuddiestMomentSolution/MuddiestMoment.Api/Program.cs
@@ -1,43 +1,28 @@
+// [X] Use Top Level Statements
+// In an API project, this is where it starts
+
+using MuddiestMoment.Api.Student;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
-
 // Add the services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+// Above this is configuration of services (things that own some state and the process around it) that we need in our application
+builder.Services.AddValidation();   // opting in to services to handle some stuff for you
 var app = builder.Build();
+// Everything here is setting up how we actually handle incoming request & write responses
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// Add the code I am about the write that allows us to handle POST to /student/moments
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapStudentEndpoints();
 
 app.MapDefaultEndpoints();
-app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+// The api is not up and running (listening for requests until we hit the next line)
+app.Run();  // "Blocking loop"
+
+
+// In .NET, when this gets compiled, it creates Assemblies. 
---
+++ src/week1/MuddiestMomentSolution/MuddiestMoment.Api/Student/ApiExtensions.cs
@@ -0,0 +1,22 @@
+﻿using MuddiestMoment.Api.Student.Endpoints;
+using System.Text.RegularExpressions;
+
+namespace MuddiestMoment.Api.Student;
+
+public static class ApiExtensions
+{
+    extension(IEndpointRouteBuilder endpoints)
+    {
+        // POST /students/moments
+        // GET /student/moments
+        public IEndpointRouteBuilder MapStudentEndpoints()
+        {
+            var group = endpoints.MapGroup("/student/moments");
+            // If any http post methods come in for /student/moments run this function
+            group.MapPost("", StudentAddsMoment.AddMoment);
+
+            return group;
+        }
+    }
+}
+
