ad45f9e8a96f3eeb08cb6d2fec4da827ca1d6c3c
AmyJane Reilly <amyjane.reilly@gmail.com>
2026-02-16 16:00:10 -0500

    today

---
+++ src/week2/MuddiestMomentSolution/MuddiestMomentTests/Student/AddsMoment.cs
@@ -0,0 +1,52 @@
+﻿
+using Alba;
+using MuddiestMoment.Api.Student.Endpoints;
+using System.Reflection;
+
+namespace MuddiestMomentTests.Student;
+
+public class AddsMoment
+{
+    [Fact]
+    public async Task CanAddMoment()
+    {
+        // Starts the program 
+        var host = await AlbaHost.For<Program>();
+
+        // Scenario
+        // Start up API
+        // Make a post request with some data to /student/moments
+        // The status code should be a 200.
+        // We should also get some stuff back
+        // Part 2 later
+
+        var itemToSend = new StudentMomentCreateModel
+        {
+            Title = "Containers",
+            Description = "Tell me about volumes"
+        };
+
+        var response = await host.Scenario(api =>
+        {
+            // Fluent Interface - a "Domain Specific Language"
+            api.Post.Json(itemToSend).ToUrl("student/moments");
+            api.StatusCodeShouldBeOk();
+
+        });
+
+
+    }
+}
+
+/*POST https://localhost:1337/student/moments
+Content-Type: application/json
+
+{
+    "title": "Containers",
+    "description": "Tell me about volumes"
+}
+
+dotnet run // start the api
+dotnet test // run my system tests.
+
+*/
